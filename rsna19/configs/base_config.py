ba6cee2b084d4fe3f6fb6d51e60db62ec2865b58
Ramstein <gerialworld@gmail.com>
2020-12-28 19:18:24 +0530

    Changes made in the code

--- rsna19/configs/base_config.py
+++ rsna19/configs/base_config.py
@@ -1,12 +1,12 @@
 class BaseConfig:
     nb_folds = 5
 
-    train_dir = '/home/ec2-user/SageMaker/rsna_dataset/stage_2_train'
-    test_dir = '/home/ec2-user/SageMaker/rsna_dataset/stage_2_test'
-    # test2_dir = '/home/ec2-user/SageMaker/rsna_dataset/stage_2_test_images'
-    labels_path = "/home/ec2-user/SageMaker/rsna_dataset/stage_1_train.csv"
+    train_dir = '/home/ec2-user/SageMaker/rsna_dataset/rsna19/stage_2_train'
+    test_dir = '/home/ec2-user/SageMaker/rsna_dataset/rsna19/stage_2_test'
+    # test2_dir = '/home/ec2-user/SageMaker/rsna_dataset/rsna19/stage_2_test_images'
+    labels_path = "/home/ec2-user/SageMaker/rsna_dataset/rsna19/stage_2_train.csv"
 
-    data_root = "/home/ec2-user/SageMaker/rsna_dataset/"
+    data_root = "/home/ec2-user/SageMaker/rsna_dataset/rsna19/"
 
     # Used for Dmytro's models
     checkpoints_dir = "/home/ec2-user/SageMaker/output/checkpoints"
